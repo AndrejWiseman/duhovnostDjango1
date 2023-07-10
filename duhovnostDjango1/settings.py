7119cf9df7da7190e65eabafcdca912bfb7eee90
A.M <andrej2000aa@gmail.com>
2023-07-10 14:51:45 +0200

    sklonjena lista

--- duhovnostDjango1/settings.py
+++ duhovnostDjango1/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'duhovnostDjango1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
